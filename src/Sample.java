9e897366ea25313da0da29d27af3ad687ae53371
Hyeonsung-Lee <sss23236@naver.com>
2024-03-20 14:50:43 +0900

    숫자

---
+++ src/Sample.java
@@ -0,0 +1,34 @@
+public class Sample {
+    public static void main(String[] args) {
+        // 숫자 연산
+        int a = 10;
+        int b = 5;
+
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+
+        System.out.println(7%3);
+        System.out.println(3%7);
+
+
+        //증감 연산
+        int i = 0;
+        int j = 10;
+        i++;
+        j--;
+
+        System.out.println(i);
+        System.out.println(j);
+
+        int q = 0;
+        System.out.println(q++); // 0 출력
+        System.out.println(q); // 1 출력
+
+        int w = 0;
+        System.out.println(++w); // 1 출력
+        System.out.println(w); // 1 출력
+
+    }
+}
