07ae1ddb465c1a69459bd0314a1fbfceeaaf4563
Hyeonsung-Lee <sss23236@naver.com>
2024-03-20 19:22:48 +0900

    24/03/20

---
+++ src/chapter3/Sample03_5.java
@@ -0,0 +1,29 @@
+package chapter3;
+
+public class Sample03_5 {
+    public static void main(String[] args) {
+
+//        StringBuffer sb = new StringBuffer();
+//        sb.append("hello");
+//        sb.append(" "); // 띄어쓰기
+//        sb.append("jump to java");
+//        String result = sb.toString();
+//        System.out.println(result);
+
+        String result = "";
+        result += "hello";
+        result += " ";
+        result += "jump to java";
+        System.out.println(result);
+
+        StringBuffer sb = new StringBuffer();
+        sb.append("jump to java");
+        sb.insert(0, "hello ");
+        System.out.println(sb.toString());
+
+        StringBuffer sb1 = new StringBuffer();
+        sb1.append("Hello jump to java");
+        System.out.println(sb1.substring(0, 4));
+
+    }
+}
