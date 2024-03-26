fddb54a0bfb3b054c8403d22e05e7385def9a0fe
Hyeonsung-Lee <sss23236@naver.com>
2024-03-26 14:38:24 +0900

    24/03/23

--- src/chapter5/Sample05_5.java
+++ src/chapter5/Sample05_5.java
@@ -1,29 +1,29 @@
 package chapter5;
 // 상속 , 메서드 오버라이딩, 오버로딩
 
-class Animal {
-    String name;
-
-    void setName(String name) {
-        this.name = name;
-    }
-}
-
-class Dog extends Animal { // Animal 클래스를 상속한다.
-    void sleep() {
-        System.out.println(this.name+" zzz");
-    }
-}
-
-class HouseDog extends Dog {
-    void sleep() {
-        System.out.println(this.name+ " zzz in house");
-    }
-
-    void sleep(int hour) {
-        System.out.println(this.name + " zzz in house for " + hour + " hours");
-    }
-}
+//class Animal {
+//    String name;
+//
+//    void setName(String name) {
+//        this.name = name;
+//    }
+//}
+//
+//class Dog extends Animal { // Animal 클래스를 상속한다.
+//    void sleep() {
+//        System.out.println(this.name+" zzz");
+//    }
+//}
+//
+////class HouseDog extends Dog {
+//    void sleep() {
+//        System.out.println(this.name+ " zzz in house");
+//    }
+//
+//    void sleep(int hour) {
+//        System.out.println(this.name + " zzz in house for " + hour + " hours");
+//    }
+//}
 
 
 public class Sample05_5 {
@@ -33,10 +33,10 @@ public class Sample05_5 {
 //        System.out.println(dog.name);
 //        dog.sleep();
 
-        HouseDog houseDog = new HouseDog();
-        houseDog.setName("happy");
-        houseDog.sleep();
-        houseDog.sleep(3);
+//        HouseDog houseDog = new HouseDog();
+//        houseDog.setName("happy");
+//        houseDog.sleep();
+//        houseDog.sleep(3);
     }
 }
 
---
+++ src/chapter5/Sample05_7.java
@@ -0,0 +1,48 @@
+package chapter5;
+// 인터페이스
+
+interface Predator {
+    String getFood();
+
+    default void printFood() {
+        System.out.printf("my food is %s\n", getFood());
+    }
+}
+class Animal {
+    String name;
+
+    void setName(String name) {
+        this.name = name;
+    }
+}
+
+class Tiger extends Animal implements Predator{
+    public String getFood() {
+        return "apple";
+    }
+
+}
+
+class Lion extends Animal implements Predator{
+    public String getFood() {
+        return "banana";
+    }
+}
+
+class ZooKeeper {
+    void feed(Predator predator) {
+        System.out.println("feed"+predator.getFood());
+    }
+}
+
+
+public class Sample05_7 {
+    public static void main(String[] args) {
+        ZooKeeper zooKeeper = new ZooKeeper();
+        Tiger tiger = new Tiger();
+        Lion lion = new Lion();
+        zooKeeper.feed(tiger);  // feed apple 출력
+        zooKeeper.feed(lion);   // feed banana 출력
+    }
+
+}
